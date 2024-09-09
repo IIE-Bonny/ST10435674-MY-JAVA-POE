3a0622cb4f787233c9a8d7e2d31f542379c4b81e
IIE-Bonny <ST10435674@rcconnect.edu.za>
2024-09-09 09:30:41 +0200

    First commit

---
+++ src/main/java/com/mycompany/poegithub/POEgithub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poegithub;
+
+/**
+ *
+ * @author B.O.N.N.Y
+ */
+public class POEgithub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
