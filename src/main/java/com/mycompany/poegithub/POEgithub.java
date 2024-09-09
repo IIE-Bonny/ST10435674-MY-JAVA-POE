8165c5b2ee6d88fc6896b251eb52130e82b770bc
IIE-Bonny <ST10435674@rcconnect.edu.za>
2024-09-09 10:25:35 +0200

    Created a program to add two numbers

--- src/main/java/com/mycompany/poegithub/POEgithub.java
+++ src/main/java/com/mycompany/poegithub/POEgithub.java
@@ -11,6 +11,9 @@ package com.mycompany.poegithub;
 public class POEgithub {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+          int a = 5;
+          int b = 10;
+          int c = a+b;
+          System.out.println("Sum: " +c);
     }
 }
